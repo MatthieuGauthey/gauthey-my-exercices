ec4a026a75d5bb97cb3b55c60ab0578570072ec3
MatthieuGauthey <matthieu.gauthey@gmail.com>
2021-10-20 14:21:42 +0200

    day1 ex0

---
+++ day-1/exercice-0.js
@@ -0,0 +1,9 @@
+export const my_sum = (a, b) => {
+    if (((typeof a) != "number") | ((typeof b) != "number")) {
+        return 0
+    }
+    if ((a == null) | (b == null)) {
+        return 0;
+    }
+    return a+b;
+}

65a6f37c5c6c625b7118a8f8331681ddbf9a6b42
MatthieuGauthey <matthieu.gauthey@gmail.com>
2021-10-21 09:56:43 +0200

    revised d1e5

--- day-1/exercice-5.js
+++ day-1/exercice-5.js
@@ -1,3 +1,9 @@
-export const my_array_alpha = (...args) => {
-    return args;
+import { length } from "../modules/length.js";
+
+export const my_array_alpha = str => {
+    var arr = [];
+    for (let i = 0; i < length(str); i++) {
+        arr[i] = str[i];
+    }
+    return arr;
 }

40aee66ae5cb6786a7df229045219a0b880ea160
MatthieuGauthey <matthieu.gauthey@gmail.com>
2021-10-21 15:50:44 +0200

    revised d2e4 and d2e6

--- day-2/exercice-4.js
+++ day-2/exercice-4.js
@@ -2,10 +2,12 @@ export const my_display_comb = () => {
     var res = [];
     var n = 0;
     for (let i = 0; i < 100; i++) {
-        for (let j = 1; j < 100; j++, n++) {
+        for (let j = i +1; j < 100; j++, n++) {
             let str = i + " " + j;
             res[n] = str;
         }
     }
     return res;
 }
+
+console.log(my_display_comb());
--- day-2/exercice-6.js
+++ day-2/exercice-6.js
@@ -3,15 +3,23 @@ import { length } from "../modules/length.js";
 export const my_display_unicode = arr => {
     var alphabet = "0123456789       ABCDEFGHIJKLMNOPQRSTUVWXYZ      abcdefghijklmnopqrstuvwxyz";
     var res = "";
+    console.log(arr);
     for (let i = 0; i < length(arr); i++) {
+        if (typeof arr[i] != "number") {
+            console.log("invalid arg");
+            continue;
+        }
         if (arr[i] === 32) {
             res += " ";
+            console.log("space");
             continue;
         }
         let c = alphabet[arr[i] - 48];
         if (c === ' ') {
+            console.log("OOB")
             continue;
         }
+        console.log("valid char");
         res += c;
     }
     return res;

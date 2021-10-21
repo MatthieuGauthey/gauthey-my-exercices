cbedf5d6bbb1bcf024cfc0770e581e31825c52f5
MatthieuGauthey <matthieu.gauthey@gmail.com>
2021-10-21 10:54:16 +0200

    revised d1e4

--- day-1/exercice-4.js
+++ day-1/exercice-4.js
@@ -1,10 +1,8 @@
+import { length } from "../modules/length.js";
+
 export const my_size_alpha = str => {
     if (typeof str != "string") {
         return 0;
     }
-    let i = 0;
-    while (str[i] != null) {
-        i++;
-    }
-    return i;
+    return length(str);
 }
